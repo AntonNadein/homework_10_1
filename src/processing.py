ebaa97b42c2d0e708dc2121b05169bdebcf08072
Anton <fynjy-666@mail.ru>
2024-07-09 20:14:48 +0300

    Revert "add README"
    This reverts commit b21f30d803c1d79fd8ea7030707b2ff22a139294.

--- src/processing.py
+++ src/processing.py
@@ -1,13 +1,4 @@
-from typing import List, Dict, Any
-
-
-def filter_by_state(list_dict: List[Dict[str, Any]], state: str = 'EXECUTED') -> List[Dict[str, Any]]:
-    '''
-    Фильтрует список транзакций по заданному состоянию
-    :param list_dict: Список словарей с данными о транзакциях.
-    :param state: Состояние транзакции для фильтрации (по умолчанию 'EXECUTED').
-    :return:Новый список, содержащий только те транзакции, у которых ключ 'state' равен переданному значению.
-    '''
+def filter_by_state(list_dict: list, state: str = "EXECUTED") -> list:
     new_list = []
 
     for index in list_dict:
@@ -16,6 +7,6 @@ def filter_by_state(list_dict: List[Dict[str, Any]], state: str = 'EXECUTED') ->
     return new_list
 
 
-def sort_by_date(list_dict: List[Dict[str, any]], sort_parameter: bool = True) -> List[Dict[str, Any]]:
+def sort_by_date(list_dict: list, sort_parameter: bool = True) -> list:
     sorted_list = sorted(list_dict, key=lambda x: x["date"], reverse=sort_parameter)
     return sorted_list
