b21f30d803c1d79fd8ea7030707b2ff22a139294
Anton <fynjy-666@mail.ru>
2024-07-09 20:14:13 +0300

    add README

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,13 @@
-def filter_by_state(list_dict: list, state: str = "EXECUTED") -> list:
+from typing import List, Dict, Any
+
+
+def filter_by_state(list_dict: List[Dict[str, Any]], state: str = 'EXECUTED') -> List[Dict[str, Any]]:
+    '''
+    Фильтрует список транзакций по заданному состоянию
+    :param list_dict: Список словарей с данными о транзакциях.
+    :param state: Состояние транзакции для фильтрации (по умолчанию 'EXECUTED').
+    :return:Новый список, содержащий только те транзакции, у которых ключ 'state' равен переданному значению.
+    '''
     new_list = []
 
     for index in list_dict:
@@ -7,6 +16,6 @@ def filter_by_state(list_dict: list, state: str = "EXECUTED") -> list:
     return new_list
 
 
-def sort_by_date(list_dict: list, sort_parameter: bool = True) -> list:
+def sort_by_date(list_dict: List[Dict[str, any]], sort_parameter: bool = True) -> List[Dict[str, Any]]:
     sorted_list = sorted(list_dict, key=lambda x: x["date"], reverse=sort_parameter)
     return sorted_list
