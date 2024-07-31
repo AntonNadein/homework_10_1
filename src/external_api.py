2758b4e6cd62c31e25a257304dcd3f28644d24ea
Anton <fynjy-666@mail.ru>
2024-07-31 13:15:42 +0300

    remade amount_transaction_rub, update conftest.py

--- src/external_api.py
+++ src/external_api.py
@@ -6,24 +6,28 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def amount_transaction_rub(amount: float, currency: str = "RUB") -> float | str:
+def amount_transaction_rub(transaction: dict) -> float | str:
     """
     Функция принимает на вход транзакцию и возвращает сумму транзакции
-    :param amount: сумма транзакции
-    :param currency: тикер валюты транзакции
+    :param transaction: словарь с транзакциями
     :return: сумма транзакции в рублях
     """
-    tuple_currency = ("USD", "CNY", "EUR", "KZT", "HKD")
-    if currency in "RUB":
-        return float(amount)
-    elif currency in tuple_currency:
-        API_KEY = os.getenv("API_KEY")
-        to = "RUB"
-        url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={currency}&amount={amount}"
-        headers = {"apikey": API_KEY}
+    try:
+        amount = transaction["operationAmount"]["amount"]
+        currency = transaction["operationAmount"]["currency"]["code"]
 
-        response = requests.get(url, headers=headers)
+        if currency == "RUB":
+            return float(amount)
+        else:
+            API_KEY = os.getenv("API_KEY")
+            to = "RUB"
+            url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={currency}&amount={amount}"
+            headers = {"apikey": API_KEY}
 
-        if response.status_code != 200:
-            return "Нет ответа"
-        return round(response.json()["result"], 2)
+            response = requests.get(url, headers=headers)
+
+            if response.status_code != 200:
+                return "Нет ответа"
+            return round(response.json()["result"], 2)
+    except KeyError:
+        return "В словаре нет ключа amount или currency"
--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -9,28 +9,28 @@ API_KEY = os.getenv('API_KEY')
 
 
 @patch('requests.get')
-def test_amount_transaction_rub(mocked_response):
+def test_amount_transaction_rub(mocked_response, dict_external_api_usd):
     mocked_response.return_value.status_code = 201
-    result = amount_transaction_rub(100, currency="USD")
+    result = amount_transaction_rub(dict_external_api_usd)
     assert result == 'Нет ответа'
 
 
 @patch('requests.get')
-def test_amount_transaction_rub_currency(mocked_get):
+def test_amount_transaction_rub_currency(mocked_get, dict_external_api_usd):
     mocked_get.return_value.status_code = 200
     mocked_get.return_value.json.return_value = {'result': 800}
-    result = amount_transaction_rub(100, currency="USD")
+    result = amount_transaction_rub(dict_external_api_usd)
     assert result == 800
     mocked_get.assert_called_once_with(
         "https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=USD&amount=100", headers={"apikey": API_KEY}
     )
 
 
-def test_amount_transaction_rub_not_currency():
-    result = amount_transaction_rub(1000)
-    assert result == 1000.00
+def test_amount_transaction_rub_not_currency(dict_external_api_rub):
+    result = amount_transaction_rub(dict_external_api_rub)
+    assert result == 31957.58
 
 
-def test_amount_transaction_rub_input_RUB():
-    result = amount_transaction_rub(1000, currency="RUB")
-    assert result == 1000.00
+def test_amount_transaction_rub_key_error():
+    result = amount_transaction_rub({})
+    assert result == "В словаре нет ключа amount или currency"
