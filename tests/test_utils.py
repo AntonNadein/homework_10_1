5c3448e7eb161776afc473c98c809de4272bcc33
Anton <fynjy-666@mail.ru>
2024-08-07 13:18:13 +0300

    upd test utils,widget

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,6 @@
+import json
+from unittest.mock import patch
+
 import pytest
 
 from src.utils import json_to_dict
@@ -17,8 +20,27 @@ def test_json_to_dict_bad_name(name, exp):
     assert test == exp
 
 
-def test_json_to_dict():
-    assert json_to_dict("operations_test.json") == [{'date': '2019-08-26T10:50:58.294041',
+@patch('json.load')
+def test_json_to_dict(mock_json):
+    test_file = [
+        {
+            "date": "2019-08-26T10:50:58.294041",
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "id": 441945886,
+            "operationAmount": {
+                "amount": "31957.58",
+                "currency": {
+                    "code": "RUB",
+                    "name": "руб."
+                }
+            },
+            "state": "EXECUTED",
+            "to": "Счет 64686473678894779589"
+        }
+    ]
+    mock_json.return_value = test_file
+    assert json_to_dict('operations.json') == [{'date': '2019-08-26T10:50:58.294041',
                                                      'description': 'Перевод организации',
                                                      'from': 'Maestro 1596837868705199',
                                                      'id': 441945886,
