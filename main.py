e2698e9b210fe510eb2759702e3ba2a1ac9354c7
Anton <fynjy-666@mail.ru>
2024-08-07 14:34:21 +0300

    add open_file.py, transaction.csv, transactions_excel.xlsx upd main, pyproject.toml

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src.masks import get_mask_account, get_mask_card_number
+from src.open_file import open_csv, open_excel
 from src.utils import json_to_dict
 
 if __name__ == '__main__':
@@ -8,3 +9,7 @@ if __name__ == '__main__':
 
 if __name__ == '__main__':
     print(json_to_dict('operations.json'))
+
+if __name__ == '__main__':
+    open_csv('transaction.csv')
+    open_excel('transactions_excel.xlsx')
